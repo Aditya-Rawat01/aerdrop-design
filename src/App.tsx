03ddebef45854a38d295f220b73cf4b85053a6a4
Aditya Rawat <adityarawat240@gmail.com>
2025-01-30 09:04:26 +0530

    one image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Hero from "./MyComp/Hero"
 import Topbar from "./MyComp/Topbar"
 
 function App() {
   return (
-    <div className="bg-primary w-screen h-screen flex justify-center">
+    <div className="bg-primary w-screen h-screen flex flex-col items-center relative">
       <Topbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/MyComp/Hero.tsx
@@ -0,0 +1,9 @@
+import drone from '../assets/droneshot.png'
+
+export default function Hero () {
+    return (
+    <div className="w-screen h-screen relative">
+        safasdfas
+        <div className='absolute h-[946px] w-[1000px] -top-10 -right-10 z-0'><img src={drone}/></div>
+    </div>)
+}
